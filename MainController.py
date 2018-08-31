6808fdce8bcfea01d14e320a7b7608acc2f4cb52
tuxta <tuxta2@gmail.com>
2018-08-31 19:37:46 +1000

    Oops, forgot the MainController >_>  <_<

---
+++ MainController.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import sys
+import pygame
+from GameFrame import Globals
+
+pygame.mixer.pre_init(44100, -16, 2, 2048)
+pygame.mixer.init()
+pygame.init()
+pygame.font.init()
+
+pygame.display.set_caption(Globals.window_name)
+window_size = (Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT)
+screen = pygame.display.set_mode(window_size,
+                                 pygame.DOUBLEBUF, 32)
+
+Globals.next_level = Globals.start_level
+levels = Globals.levels
+
+# - Main Game Loop. Steps through the levels defined in levels[] - #
+while Globals.running:
+
+    curr_level = Globals.next_level
+    Globals.next_level += 1
+    Globals.next_level %= len(levels)
+    mod_name = "Rooms.{}".format(levels[curr_level])
+    mod = __import__(mod_name)
+    class_name = getattr(mod, levels[curr_level])
+    room = class_name(screen)
+    exit_val = room.run()
+
+    if exit_val is True or Globals.running is False:
+
+        Globals.next_level = Globals.end_game_level
+
+        if len(levels) == 1:
+            break
+
+    if Globals.exiting:
+        break
+
+sys.exit()
