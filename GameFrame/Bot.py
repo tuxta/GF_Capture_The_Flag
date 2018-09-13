08c3388122f67366ae9092287644a976bc647a7e
tuxta <tuxtatwo@gmail.com>
2018-09-14 09:21:54 +1000

    Fixed the rotation issue when moving left or right towards a coordinate

--- GameFrame/Bot.py
+++ GameFrame/Bot.py
@@ -26,40 +26,36 @@ class Bot(RoomObject):
 
     def turn_left(self, speed=Globals.SLOW):
         if speed == Globals.FAST:
-            self.rotate(-9)
-        elif speed == Globals.MEDIUM:
-            self.rotate(-6)
-        else:
-            self.rotate(-3)
-
-    def turn_right(self, speed=Globals.SLOW):
-        if speed == Globals.FAST:
             self.rotate(9)
         elif speed == Globals.MEDIUM:
             self.rotate(6)
         else:
             self.rotate(3)
 
+    def turn_right(self, speed=Globals.SLOW):
+        if speed == Globals.FAST:
+            self.rotate(-9)
+        elif speed == Globals.MEDIUM:
+            self.rotate(-6)
+        else:
+            self.rotate(-3)
+
     def turn_towards(self, x, y, speed=Globals.SLOW):
-        if self.curr_rotation > 360:
-            self.curr_rotation = self.curr_rotation - 360
-        elif self.curr_rotation < 0:
-            self.curr_rotation = 350 - self.curr_rotation
-            
-        target_angle = self.get_rotation_to_coordinate(x, y)
-        target_angle += 180
-        if target_angle >= 360:
-            target_angle -= 360
+
+        target_angle = int(self.get_rotation_to_coordinate(x, y))
+
+        if target_angle < 0:
+            target_angle = 180 - abs(target_angle) + 180
 
         if self.curr_rotation <= 180:
             if self.curr_rotation + 2 < target_angle < self.curr_rotation + 180:
                 self.turn_left(speed)
-            elif self.curr_rotation - 2 < target_angle <= 0 or self.curr_rotation + 180 < target_angle < 360:
+            else:
                 self.turn_right(speed)
         else:
             if self.curr_rotation + 2 < target_angle < 360 or 0 <= target_angle < self.curr_rotation - 180:
                 self.turn_left(speed)
-            elif self.curr_rotation - 2 < target_angle <= self.curr_rotation - 180:
+            else:
                 self.turn_right(speed)
 
     def drive_forward(self, speed=Globals.SLOW):
